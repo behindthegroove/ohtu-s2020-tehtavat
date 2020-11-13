234de7457dca61f7ed2bef297ece1dbc9a65a47f
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-13 21:46:56 +0200

    Update nhlreader

--- viikko3/tehtavat1-3/nhlreader/src/main/java/ohtu/Player.java
+++ viikko3/tehtavat1-3/nhlreader/src/main/java/ohtu/Player.java
@@ -3,6 +3,12 @@ package ohtu;
 
 public class Player {
     private String name;
+    private String nationality;
+    private int assists;
+    private int goals;
+    private int penalties;
+    private String team;
+    private int games;
 
     public void setName(String name) {
         this.name = name;
@@ -10,6 +16,54 @@ public class Player {
 
     public String getName() {
         return name;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public int getGames() {
+        return games;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
     }
 
     @Override
