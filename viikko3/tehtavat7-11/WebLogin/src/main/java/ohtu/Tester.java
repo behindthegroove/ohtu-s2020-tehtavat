f1eb339253460ca9b2b87ccd404bf2fc3ed72451
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-16 13:54:54 +0200

    Add HtmlUnitDriver

--- viikko3/tehtavat7-11/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/tehtavat7-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,18 +4,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Tester {
 
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new HtmlUnitDriver();
 
         driver.get("http://localhost:4567");
-        
+        System.out.println(driver.getPageSource()); // tulostetaan sivu konsoliin
+
         sleep(2);
-        
+
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
+        System.out.println(driver.getPageSource()); // tulostetaan sivu konsoliin
 
         sleep(2);
 
@@ -24,12 +27,16 @@ public class Tester {
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-        
+        System.out.println(driver.getPageSource()); // tulostetaan sivu konsoliin
+
         sleep(2);
         element.submit();
+        System.out.println(driver.getPageSource()); // tulostetaan sivu konsoliin
 
         sleep(3);
-        
+
+        System.out.println(driver.getPageSource()); // tulostetaan sivu konsoliin
+
         driver.quit();
     }
     
