c9de9e677661b0ed73d20d89593e03588c7689bc
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-16 15:07:08 +0200

    Add nonexistent user scenario

--- viikko3/tehtavat7-11/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ viikko3/tehtavat7-11/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -42,7 +42,12 @@ public class Stepdefs {
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
         pageHasContent("invalid username or password");
         pageHasContent("Give your credentials to login");
-    }    
+    }
+
+    @When("nonexistent username {string} and password {string} are given")
+    public void nonexistentUsernameIsGiven(String username, String password) {
+        logInWith(username, password);
+    }
     
     @After
     public void tearDown(){
