2ee7a1cd6f3e85ee9991b1f0fc3a9d5567cbc537
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-19 16:31:47 +0200

    Add tests

--- viikko4/tehtavat1-4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
+++ viikko4/tehtavat1-4/MaksukorttiMockito/src/test/java/ohtu/matkakortti/KassapaateTest.java
@@ -40,5 +40,17 @@ public class KassapaateTest {
         verify(kortti, times(1)).getSaldo();
         verify(kortti, times(0)).osta(anyInt());
     }
-      
+
+    @Test
+    public void kortilleLataaminenToimiiJosLadattavaPositiivinen() {
+        kassa.lataa(kortti, 10);
+        verify(kortti, times(1)).lataa(eq(10));
+    }
+
+    @Test
+    public void kortilleEiLadataJosLadattavaNegatiivinen() {
+        kassa.lataa(kortti, -10);
+        verify(kortti, times(0)).lataa(anyInt());
+    }
+
 }
