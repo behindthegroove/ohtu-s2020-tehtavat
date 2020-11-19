658e32b3be42433522e84257b2d16ddef6d26116
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-19 16:35:28 +0200

    Fix Kassapaate

--- viikko4/tehtavat1-4/MaksukorttiMockito/src/main/java/ohtu/matkakortti/Kassapaate.java
+++ viikko4/tehtavat1-4/MaksukorttiMockito/src/main/java/ohtu/matkakortti/Kassapaate.java
@@ -9,15 +9,16 @@ public class Kassapaate {
     }
     
     public void lataa(Maksukortti kortti, int summa){
-        kortti.lataa(summa);
+        if (summa >= 0) {
+            kortti.lataa(summa);
+        }
     }
     
     public void ostaLounas(Maksukortti kortti) {
-        if (kortti.getSaldo() < HINTA) {
-            return;
+        if (kortti.getSaldo() >= HINTA) {
+            kortti.osta(HINTA);
+            myytyjaLounaita++;
         }
-        kortti.osta(HINTA);
-        myytyjaLounaita++;
     }
 
     public int getMyytyjaLounaita() {
