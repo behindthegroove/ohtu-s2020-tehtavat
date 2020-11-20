c8820571d793d832b94620a6b83ef3f1e074c57c
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-11-20 17:56:06 +0200

    Refactor TennisGame

--- viikko4/tehtavat5-6/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko4/tehtavat5-6/Tennis/src/main/java/ohtu/TennisGame.java
@@ -2,79 +2,94 @@ package ohtu;
 
 public class TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+    private int score1 = 0;
+    private int score2 = 0;
+    private String player1;
+    private String player2;
 
-    public TennisGame(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+    public TennisGame(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
     }
 
-    public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
+    public void wonPoint(String player) {
+        if (player == "player1")
+            score1 += 1;
         else
-            m_score2 += 1;
+            score2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                case 3:
-                        score = "Forty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
+
+        if (score1==score2) {
+            score = getEqualScore();
+        } else if (score1>=4 || score2>=4) {
+            score = getWinOrAdvantage();
+        } else {
+            score = getPlayerScores();
         }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
-            }
-        }
+
         return score;
     }
+
+    private String getEqualScore() {
+        switch (score1) {
+            case 0:
+                return "Love-All";
+            case 1:
+                return "Fifteen-All";
+            case 2:
+                return "Thirty-All";
+            case 3:
+                return "Forty-All";
+            default:
+                return "Deuce";
+        }
+    }
+
+    private String getWinOrAdvantage() {
+        int scoreDifference = score1 - score2;
+
+        if (scoreDifference == 1)
+            return "Advantage player1";
+        else if (scoreDifference == -1)
+            return "Advantage player2";
+        else if (scoreDifference >= 2)
+            return "Win for player1";
+        else
+            return "Win for player2";
+    }
+
+    private String getPlayerScores() {
+        String score = "";
+
+        int playerScore = 0;
+        for (int i = 1; i < 3; i++) {
+            if (i == 1) {
+                playerScore = score1;
+            } else {
+                score += "-";
+                playerScore = score2;
+            }
+
+            switch (playerScore) {
+                case 0:
+                    score += "Love";
+                    break;
+                case 1:
+                    score += "Fifteen";
+                    break;
+                case 2:
+                    score += "Thirty";
+                    break;
+                case 3:
+                    score += "Forty";
+                    break;
+            }
+        }
+
+        return score;
+    }
+
 }
