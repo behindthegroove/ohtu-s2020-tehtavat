fcd9e723142a0b8577911e97144c6b730862f980
Emma Valtonen <emma.valtonen@helsinki.fi>
2020-12-07 17:55:32 +0200

    Add matchers All, Not and HasFewerThan

--- viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/tehtavat2-5/QueryLanguage/src/main/java/statistics/Main.java
@@ -16,5 +16,28 @@ public class Main {
         for (Player player : stats.matches(m)) {
             System.out.println(player);
         }
+        System.out.println();
+
+        m = new And(
+                new Not(new HasAtLeast(1, "goals")),
+                new PlaysIn("NYR")
+        );
+
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+        System.out.println();
+
+        m = new And(
+                new HasFewerThan(1, "goals"),
+                new PlaysIn("NYR")
+        );
+
+        for (Player player : stats.matches(m)) {
+            System.out.println(player);
+        }
+        System.out.println();
+
+        System.out.println(stats.matches(new All()).size());
     }
 }
